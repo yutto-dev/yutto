f1245e4e4f415331c871d4f8a95bced40187becf
SigureMo <sigure.qaq@gmail.com>
2021-05-08 23:00:18 +0800

    💬 refactor: update danmaku default value to ass

--- yutto/cli/basic.py
+++ yutto/cli/basic.py
@@ -28,7 +28,7 @@ def add_basic_arguments(parser: argparse.ArgumentParser):
     )
     parser.add_argument("--only-video", dest="require_audio", action="store_false", help="只下载视频")
     parser.add_argument("--only-audio", dest="require_video", action="store_false", help="只下载音频")
-    parser.add_argument("--danmaku", default="xml", choices=["xml", "ass", "no"], help="弹幕类型，no 为不生成弹幕")
+    parser.add_argument("--danmaku", default="ass", choices=["xml", "ass", "no"], help="弹幕类型，no 为不生成弹幕")
     parser.add_argument("-b", "--block-size", default=0.5, type=float, help="分块下载时各块大小，单位为 MiB，默认为 0.5MiB")
     parser.add_argument("-w", "--overwrite", action="store_true", help="强制覆盖已下载内容")
     parser.add_argument("-x", "--proxy", default="auto", help="设置代理（auto 为系统代理、no 为不使用代理、当然也可以设置代理值）")
