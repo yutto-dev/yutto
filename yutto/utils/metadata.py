c7023b2b89a4d04182bf5584b58db391d78c019f
SigureMo <sigure.qaq@gmail.com>
2022-02-06 13:47:07 +0800

    🚨 chore: lint error

--- yutto/utils/metadata.py
+++ yutto/utils/metadata.py
@@ -2,7 +2,7 @@ import os
 from typing import TypedDict
 from xml.dom.minidom import parseString
 
-import dicttoxml
+import dicttoxml  # type: ignore
 
 
 class MetaData(TypedDict):
@@ -21,8 +21,8 @@ def write_metadata(metadata: MetaData, video_path: str):
     metadata_path = video_path_no_ext + ".nfo"
     custom_root = "episodedetails"
 
-    xml_content = dicttoxml.dicttoxml(metadata, custom_root=custom_root, attr_type=False)
-    dom = parseString(xml_content)
-    pretty_content = dom.toprettyxml()
-    with open(metadata_path, "w", encoding="utf-8") as f:
-        f.write(pretty_content)
+    xml_content = dicttoxml.dicttoxml(metadata, custom_root=custom_root, attr_type=False)  # type: ignore
+    dom = parseString(xml_content)  # type: ignore
+    pretty_content = dom.toprettyxml()  # type: ignore
+    with open(metadata_path, "w", encoding="utf-8") as f:  # type: ignore
+        f.write(pretty_content)  # type: ignore
