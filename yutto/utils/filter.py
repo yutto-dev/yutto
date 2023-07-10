318a05f33bce44f8b8108219f0e98c18a6edd453
symant233 <symant233@gmail.com>
2023-07-10 10:18:44 +0800

    fix: datetime issue

--- yutto/utils/filter.py
+++ yutto/utils/filter.py
@@ -9,7 +9,7 @@ from yutto.utils.console.logger import Logger
 class Filter:
     # NOTE(FrankHB): A workaround to https://bugs.python.org/issue31212.
     batch_filter_start_time: datetime.datetime = datetime.datetime(1971, 1, 1)
-    batch_filter_end_time: datetime.datetime = datetime.datetime.today()
+    batch_filter_end_time: datetime.datetime = datetime.datetime.now() + datetime.timedelta(days=1)
 
     @staticmethod
     def set_timer(key: str, user_input: str):
