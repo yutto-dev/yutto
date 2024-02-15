102bb20976397f75a90e265e70ffe018fcf91b71
Nyakku Shigure <sigure.qaq@gmail.com>
2024-02-15 13:47:45 +0800

    🐛 fix: add data check for subtitle (#234)

--- yutto/utils/funcutils/__init__.py
+++ yutto/utils/funcutils/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from .aobject import aobject
 from .as_sync import as_sync
+from .data_access import data_has_chained_keys
 from .filter_none_value import filter_none_value
 from .singleton import Singleton
 from .xmerge import xmerge
@@ -12,4 +13,5 @@ __all__ = [
     "as_sync",
     "filter_none_value",
     "xmerge",
+    "data_has_chained_keys",
 ]
