ade68cb7515c1c7ca84e6e62d221720fd6a1009f
SigureMo <sigure.qaq@gmail.com>
2024-09-15 03:58:22 +0800

    ⬆️ deps: bump protobuf to 5.28.1

--- packages/biliass/src/biliass/protobuf/danmaku_pb2.pyi
+++ packages/biliass/src/biliass/protobuf/danmaku_pb2.pyi
@@ -1,30 +1,12 @@
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import (
-    ClassVar as _ClassVar,
-    Iterable as _Iterable,
-    Mapping as _Mapping,
-    Optional as _Optional,
-    Union as _Union,
-)
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class DanmakuElem(_message.Message):
-    __slots__ = (
-        "id",
-        "progress",
-        "mode",
-        "fontsize",
-        "color",
-        "mid_hash",
-        "content",
-        "ctime",
-        "action",
-        "pool",
-        "id_str",
-    )
+    __slots__ = ("id", "progress", "mode", "fontsize", "color", "mid_hash", "content", "ctime", "action", "pool", "id_str")
     ID_FIELD_NUMBER: _ClassVar[int]
     PROGRESS_FIELD_NUMBER: _ClassVar[int]
     MODE_FIELD_NUMBER: _ClassVar[int]
@@ -47,25 +29,10 @@ class DanmakuElem(_message.Message):
     action: str
     pool: int
     id_str: str
-    def __init__(
-        self,
-        id: _Optional[int] = ...,
-        progress: _Optional[int] = ...,
-        mode: _Optional[int] = ...,
-        fontsize: _Optional[int] = ...,
-        color: _Optional[int] = ...,
-        mid_hash: _Optional[str] = ...,
-        content: _Optional[str] = ...,
-        ctime: _Optional[int] = ...,
-        action: _Optional[str] = ...,
-        pool: _Optional[int] = ...,
-        id_str: _Optional[str] = ...,
-    ) -> None: ...
+    def __init__(self, id: _Optional[int] = ..., progress: _Optional[int] = ..., mode: _Optional[int] = ..., fontsize: _Optional[int] = ..., color: _Optional[int] = ..., mid_hash: _Optional[str] = ..., content: _Optional[str] = ..., ctime: _Optional[int] = ..., action: _Optional[str] = ..., pool: _Optional[int] = ..., id_str: _Optional[str] = ...) -> None: ...
 
 class DanmakuEvent(_message.Message):
     __slots__ = ("elems",)
     ELEMS_FIELD_NUMBER: _ClassVar[int]
     elems: _containers.RepeatedCompositeFieldContainer[DanmakuElem]
-    def __init__(
-        self, elems: _Optional[_Iterable[_Union[DanmakuElem, _Mapping]]] = ...
-    ) -> None: ...
+    def __init__(self, elems: _Optional[_Iterable[_Union[DanmakuElem, _Mapping]]] = ...) -> None: ...
