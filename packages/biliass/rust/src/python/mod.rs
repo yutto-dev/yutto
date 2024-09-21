a2d05028daffb96e5f5ee5f02c5766e74806b49c
Nyakku Shigure <sigure.qaq@gmail.com>
2024-09-22 04:18:23 +0800

    🔥 refactor: cleanup unused pyo3 APIs (#327)

--- packages/biliass/rust/src/python/mod.rs
+++ packages/biliass/rust/src/python/mod.rs
@@ -3,11 +3,10 @@ mod proto;
 mod reader;
 mod writer;
 
-pub use comment::{PyComment, PyCommentPosition, PyOptionComment};
+pub use comment::{PyComment, PyCommentPosition};
 pub use proto::{PyDanmakuElem, PyDmSegMobileReply};
 pub use reader::{py_read_comments_from_protobuf, py_read_comments_from_xml};
 pub use writer::{
     py_ass_escape, py_convert_color, py_convert_flash_rotation, py_convert_timestamp,
-    py_find_alternative_row, py_get_zoom_factor, py_mark_comment_row, py_test_free_rows,
-    py_write_comment, py_write_head, py_write_normal_comment, PyRows,
+    py_get_zoom_factor, py_write_head, py_write_normal_comment, PyRows,
 };
