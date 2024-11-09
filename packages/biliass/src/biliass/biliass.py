83436462e54dd3d3d25cbec0fea17adfb5c844e1
Nyakku Shigure <sigure.qaq@gmail.com>
2024-11-09 23:07:07 +0800

    💥 feat!: remove deprecated API `Danmaku2ASS` (#385)

--- packages/biliass/src/biliass/biliass.py
+++ packages/biliass/src/biliass/biliass.py
@@ -10,47 +10,9 @@ from biliass._core import (
 )
 
 if TYPE_CHECKING:
-    from collections.abc import Callable, Sequence
+    from collections.abc import Sequence
 
 T = TypeVar("T")
-
-
-def Danmaku2ASS(
-    inputs: Sequence[str | bytes] | str | bytes,
-    stage_width: int,
-    stage_height: int,
-    input_format: str = "xml",
-    reserve_blank: int = 0,
-    font_face: str = "sans-serif",
-    font_size: float = 25.0,
-    text_opacity: float = 1.0,
-    duration_marquee: float = 5.0,
-    duration_still: float = 5.0,
-    comment_filter: str | None = None,
-    is_reduce_comments: bool = False,
-    progress_callback: Callable[[int, int], None] | None = None,
-) -> str:
-    print(
-        "Function `Danmaku2ASS` is deprecated in biliass 2.0.0 and will be removed in 2.2.0, Please use `convert_to_ass` instead."
-    )
-    if progress_callback is not None:
-        print("`progress_callback` will take no effect in 2.0.0")
-    comment_filters: list[str] = [comment_filter] if comment_filter is not None else []
-    block_options = BlockOptions(False, False, False, False, False, False, comment_filters)
-    return convert_to_ass(
-        inputs,
-        stage_width,
-        stage_height,
-        input_format,
-        1 - reserve_blank / stage_height,
-        font_face,
-        font_size,
-        text_opacity,
-        duration_marquee,
-        duration_still,
-        block_options,
-        is_reduce_comments,
-    )
 
 
 def convert_to_ass(
